3256eb8a7dbb352f5b8a9cb32c15f85bb9132993
Joseph Keane <joseph_keane@live.co.uk>
2019-09-06 21:08:24 +0100

    Added Switch Command

--- src/commands/switch.go
+++ src/commands/switch.go
@@ -1,12 +1,22 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
 	git "github.com/libgit2/git2go"
+	"gitwrapper"
 )
 
-func execSwitch(_ *git.Repository, _ []string, _ map[string]string) error {
-	return nil
+func execSwitch(repo *git.Repository, positionals []string, _ map[string]string) error {
+	if len(positionals) < 1 {
+		return errors.New("Line name required.")
+	}
+	if len(positionals) > 1 {
+		return errors.New("Unexpected argument: " + positionals[1])
+	}
+	name := positionals[0]
+
+	return gitwrapper.CheckoutBranch(name, repo)
 }
 
 func printSwitchHelp(_ []string, _ map[string]string) {
--- src/gitwrapper/gitwrapper.go
+++ src/gitwrapper/gitwrapper.go
@@ -111,3 +111,39 @@ func CreateBranch(name string, repo *git.Repository) (*git.Branch, error) {
 
 	return branch, nil
 }
+
+// Checks out the given branch by name
+// name - Plain Text branch name (e.g. 'master')
+// repo - Repo to checkout from
+func CheckoutBranch(name string, repo *git.Repository) error {
+	err := checkout(name, repo)
+	if err != nil {return err}
+
+	err = moveHead(name, repo)
+	return err
+}
+
+// Moves the head to the given branch
+// Files are not changed
+func moveHead(name string, repo *git.Repository) error {
+	branch, err := repo.LookupBranch(name, git.BranchLocal)
+	if err != nil {return err}
+
+	err = repo.SetHead(branch.Reference.Name())
+	return err
+}
+
+// Checks out the given branch without moving head
+// Doesn't change current branch tag
+func checkout(name string, repo *git.Repository) error {
+	commit, err := getCommit(name, repo)
+	if err != nil {return err}
+	tree, err := commit.Tree()
+	if err != nil {return err}
+
+	checkoutOps := git.CheckoutOpts{}
+	checkoutOps.Strategy = git.CheckoutSafe
+	err = repo.CheckoutTree(tree, &checkoutOps)
+
+	return err
+}
