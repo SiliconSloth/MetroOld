5c1503d1307dcabd3fac9bb1a806761392c1ba49
Joseph Keane <joseph_keane@live.co.uk>
2019-09-15 16:56:42 +0100

    Very Basic Working Pull
    Removed Github password for obvious reasons

--- src/commands/sync.go
+++ src/commands/sync.go
@@ -1,12 +1,13 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
 	git "github.com/libgit2/git2go"
+	"gitwrapper"
 )
 
 func execSync(repo *git.Repository, positionals []string, options map[string]string) error {
-	fmt.Println(options["timeout"])
 	//_, cred := git.NewCredDefault()
 	remotes, err := repo.Remotes.List()
 	if err != nil { return err }
@@ -22,15 +23,41 @@ func execSync(repo *git.Repository, positionals []string, options map[string]str
 		remotes, err = repo.Remotes.List()
 	}
 	if len(remotes) < 1 {
-		fmt.Println("Could not find given remote")
+		return errors.New("Could not find given remote.")
 	}
 	remoteStr := remotes[0]
+	err = repo.Remotes.SetUrl("origin", "https://github.com/Black-Photon/Metro-Test")
+	if err != nil { return err }
 	remote, err := repo.Remotes.Lookup(remoteStr)
 	if err != nil { return err }
 
-	fetchOps := git.FetchOptions{}
-	err = remote.Fetch(nil, &fetchOps, "Fetch")
+	callbacks := git.RemoteCallbacks{}
+	callbacks.TransferProgressCallback = func(stats git.TransferProgress) git.ErrorCode {
+		println("Indexed Objects:", stats.IndexedObjects)
+		println("Local Objects:", stats.LocalObjects)
+		println("Received Bytes:", stats.ReceivedBytes)
+		println("Received Objects:", stats.ReceivedObjects)
+		println("Total Deltas:", stats.TotalDeltas)
+		println("Total Objects:", stats.TotalObjects)
+
+		return git.ErrOk
+	}
+	callbacks.CredentialsCallback = func(url string, username_from_url string, allowed_types git.CredType) (code git.ErrorCode, cred *git.Cred) {
+		_, creds := git.NewCredUserpassPlaintext("Black-Photon", "")
+		return git.ErrOk, &creds
+	}
+	fetchOps := git.FetchOptions{ RemoteCallbacks: callbacks }
+	err = remote.Fetch(nil, &fetchOps, "pull")
 	if err != nil { return err }
+
+	conflicts, err := gitwrapper.Merge("origin/master", repo)
+	if err != nil { return err }
+
+	if !conflicts {
+		println("Successfully Downsynched")
+	} else {
+		println("Conflicts Found")
+	}
 
 	return nil
 }
