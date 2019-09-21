b95ef5daf46ecdcfdf3cf73f8efc4f5d900c5d96
SiliconSloth <siliconsloth64@gmail.com>
2019-09-21 20:53:51 +0100

    Basic C wrapper for metro.Create

--- src/library/library.go
+++ src/library/library.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
-	println("I'm a library lol")
+/*
+#include <git2.h>
+#include <git2/sys/repository.h>
+*/
+import "C"
+import (
+	"metro"
+	"reflect"
+)
+
+//export create
+func create(directory *C.char, repo *C.git_repository) C.int {
+	dirGo := C.GoString(directory)
+
+	goRepo, _ := metro.Create(dirGo)
+
+	value := reflect.ValueOf(goRepo)
+	repo = value.FieldByName("ptr").Interface().(*C.git_repository)
+	return 0
 }
+
+func main() {}
